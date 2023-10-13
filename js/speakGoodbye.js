1f7098a30b75ae02441a29872dd6d135a6cb0aff
Zahra <zahrashokouh63@gmail.com>
2023-10-13 10:11:17 +0330

    gh-pages

--- js/speakGoodbye.js
+++ js/speakGoodbye.js
@@ -1,7 +1,9 @@
-
-var speakWord = "Good Bye";
-
-function speak(name) {
-  console.log(speakWord + " " + name);
-}
+(function(window) {	
+	var speakWord = "Good Bye";
+	var byeSpeaker = {};	
+	byeSpeaker. speak = function (name) {
+		console.log(speakWord + " " + name);
+	}
+	window.byeSpeaker =  byeSpeaker;
+})(window);
 
